68ed697b4d8e9c7c5418a845e4b6516a4348ed0f
yiyuchen-nz <yiyuchen9@gmail.com>
2022-05-12 18:19:30 +1200

    inflating unicorns

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,30 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+// array AllUnicorns = [0, 1, 2]
+let allUnicorns = document.getElementsByClassName('inflate-an-image')
+//console.log(allUnicorns)
+
+// set up onclick for each unicorns in the array
+for (i = 0; i < allUnicorns.length; i++) {
+  allUnicorns[i].onclick = inflating
+}
+
+function inflating(e) {
+  let allUnicorns = e.target
+
+  if (allUnicorns.src.match('images/unicorn-0.png')) {
+    allUnicorns.src = 'images/unicorn-1.png'
+  } else if (allUnicorns.src.match('images/unicorn-1.png')) {
+    allUnicorns.src = 'images/unicorn-2.png'
+  } else if (allUnicorns.src.match('images/unicorn-2.png')) {
+    allUnicorns.src = 'images/unicorn-3.png'
+  } else if (allUnicorns.src.match('images/unicorn-3.png')) {
+    alert('Unicorn Number ' + e.target.id + ' says thank you!')
+  } else {
+    allUnicorns.src = 'images/unicorn-0.png'
+  }
+}
